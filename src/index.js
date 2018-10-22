06016f404e3938479914ac03588e3937ea3d5f06
Chris DeMartini <chris@datablick.com>
2018-10-21 22:37:23 -0700

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AppWrapper from './AppWrapper';
-import registerServiceWorker from './registerServiceWorker';
-import "typeface-roboto";
-
-ReactDOM.render(<AppWrapper />, document.getElementById('root'));
-registerServiceWorker();
